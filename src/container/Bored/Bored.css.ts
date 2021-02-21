26a9addbfa460db58fa27ea10916e55bb8c94589
Krystian <szot151@gmail.com>
2021-02-21 16:21:03 +0100

    Bored component done

---
+++ src/container/Bored/Bored.css.ts
@@ -0,0 +1,68 @@
+import styled from 'styled-components';
+
+export const Container = styled.div<{ mode: boolean }>`
+  display: flex;
+  flex-direction: column;
+  flex-basis: 100%;
+  padding: 2rem 1rem;
+  .bored {
+    &__header {
+      color: ${({ theme, mode }) => (mode ? 'white' : 'black')};
+      text-align: center;
+      &--primary {
+        font-size: 3rem;
+        font-weight: 700;
+      }
+      &--secondary {
+        font-size: 2rem;
+        color: ${({ theme, mode }) =>
+          mode ? 'white' : theme.colors.gray.normal2};
+      }
+    }
+
+    &__main {
+      margin: 5%;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+    }
+
+    &__img {
+      width: 20rem;
+      height: 20rem;
+    }
+    &__randomActiv {
+      width: 29rem;
+      min-height: 20rem;
+      border: 2px solid
+        ${({ mode, theme }) => (mode ? 'white' : theme.colors.gray.normal)};
+      border-radius: 1rem;
+      background-color: ${({ theme, mode }) =>
+        mode ? theme.colors.blue.normal : 'white'};
+      display: flex;
+      flex-direction: column;
+      padding: 2rem 1rem;
+      align-items: center;
+      margin-right: 20%;
+      position: relative;
+      &--text {
+        margin-top: 5%;
+        font-size: 2.6rem;
+        color: ${({ theme, mode }) =>
+          mode ? 'white' : theme.colors.gray.normal2};
+        text-align: center;
+      }
+    }
+  }
+
+  .customSpinner {
+    font-size: 5rem;
+    position: absolute;
+    top: 50%;
+    left: 45%;
+    transform: translate(-50%, -50%);
+  }
+  .itemCenter {
+    text-align: center;
+  }
+`;
--- src/container/Bored/Bored.tsx
+++ src/container/Bored/Bored.tsx
@@ -1,10 +1,75 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Container } from './Bored.css';
+import { Button } from '../../components';
+import { Shape } from '../../helpers';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { useSelector } from 'react-redux';
+import { AppState } from '../../data/store';
+import boredCat from '../../img/boredCat.svg';
 
 const Bored = (props: any) => {
+  const isMode = useSelector((state: AppState) => state.mode.isMode);
+  const [randomActivText, setRandomActivText] = useState<string>('');
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    async function fetchRandomText() {
+      const randomText = await onFetchData();
+      setRandomActivText(randomText);
+    }
+    fetchRandomText();
+    setIsLoading(false);
+  }, []);
+
+  const onFetchData = async (): Promise<string> => {
+    return fetch('http://www.boredapi.com/api/activity/')
+      .then((response) => response.json())
+      .then((result) => {
+        console.log('Work');
+        return result.activity;
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const onClickButton = async () => {
+    setIsLoading(true);
+    const tmp = await onFetchData();
+    console.log(tmp);
+    setRandomActivText(tmp);
+    setIsLoading(false);
+  };
   return (
-    <div>
-      <h1>Bored</h1>
-    </div>
+    <Container mode={isMode}>
+      <div className='bored__header'>
+        <h2 className='bored__header--primary'>Feeling Bored ?</h2>
+        <p className='bored__header--secondary'>
+          Don't worry. I can help you find something to do.
+        </p>
+      </div>
+      <div className='bored__main'>
+        <img src={boredCat} alt='Zdjecie' className='bored__img' />
+        <div className='bored__randomActiv'>
+          <div className='itemCenter'>
+            {' '}
+            <Button variant={Shape.square} clicked={onClickButton}>
+              Random activity
+            </Button>
+          </div>
+
+          {!isLoading ? (
+            <p className='bored__randomActiv--text'>{randomActivText}</p>
+          ) : null}
+          {isLoading ? (
+            <FontAwesomeIcon
+              icon={faSpinner}
+              className='fa-spin customSpinner'
+            />
+          ) : null}
+        </div>
+      </div>
+    </Container>
   );
 };
 
